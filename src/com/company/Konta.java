cf08cf84caae55eabea4902948f0d6df88b5a03c
Norbert731 <norbertguzy@o2.pl>
2021-12-10 20:01:34 +0100

    10.12

---
+++ src/com/company/Konta.java
@@ -0,0 +1,58 @@
+package com.company;
+
+public class Konta {
+
+    String login;
+    String Imie;
+    String Nazwisko;
+    String NumerKarty;
+    String Haslo;
+
+    public Konta(String login,String imie, String nazwisko, String numerKarty, String haslo) {
+        this.login=login;
+        Imie = imie;
+        Nazwisko = nazwisko;
+        NumerKarty = numerKarty;
+        Haslo = haslo;
+    }
+
+    public String getImie() {
+        return Imie;
+    }
+
+    public void setImie(String imie) {
+        Imie = imie;
+    }
+
+    public String getNazwisko() {
+        return Nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        Nazwisko = nazwisko;
+    }
+
+    public String getNumerKarty() {
+        return NumerKarty;
+    }
+
+    public void setNumerKarty(String numerKarty) {
+        NumerKarty = numerKarty;
+    }
+
+    public String getHaslo() {
+        return Haslo;
+    }
+
+    public void setHaslo(String haslo) {
+        Haslo = haslo;
+    }
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,77 @@
 package com.company;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Rower> rowery = wypiszRowery();
+        ArrayList<Konta> konta =  pobierzDane();
+        start();
+
+    }
+
+    private static void start() throws FileNotFoundException {
+        System.out.println("1. Logowanie");
+        System.out.println("2. Rejestracja");
+        System.out.println("3. Wyjscie");
+        System.out.print("Wybierz co chcesz zrobic wariacie: ");
+        Scanner scanner = new Scanner(System.in);
+        int wybor = scanner.nextInt();
+        if (wybor==1){
+            logowanie();
+        }
+        else if(wybor==2){
+            rejestrcja();
+        }
+        else{
+            System.exit(0);}
+    }
+
+    private static void rejestrcja() {
+
+    }
+
+    private static void logowanie() throws FileNotFoundException {
+        ArrayList<Konta> konta =  pobierzDane();
+        Scanner scanner = new Scanner(System.in);
+        String login="";
+        String haslo="";
+        System.out.println("Naciśnij 1 aby przejść do logowanie lub 2 aby wrócić");
+        if(scanner.nextInt()==1){
+            System.out.print("Podaj login: ");
+            login= scanner.next();
+            System.out.print("Podaj haslo: ");
+            haslo= scanner.next();
+           }
+        else start();
+        for (Konta k:konta) {
+            if(k.getLogin().equals(login) && k.getHaslo().equals(haslo)){
+                System.out.println("Gejtuaslcka");
+            }//Popraw to
+            else{
+                System.out.println("Błędne dane");
+                logowanie();
+            }
+        }
+        scanner.close();
+    }
+
+    private static ArrayList<Konta> pobierzDane() throws FileNotFoundException {
+        ArrayList<Konta> konta = new ArrayList<>();
+        File file = new File("src/konta.txt");
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNext()){
+            Konta konto = new Konta(scanner.next(),scanner.next(),scanner.next(), scanner.next(), scanner.next());
+            konta.add(konto);
+        }
+        scanner.close();
+        return konta;
+    }
+    public static ArrayList<Rower>  wypiszRowery() throws FileNotFoundException {
         File file = new File("src/rowery.txt");
         Scanner scanner = new Scanner(file);
         ArrayList<Rower> rowers = new ArrayList<>();
@@ -16,15 +80,7 @@ public class Main {
                     scanner.nextBoolean());
             rowers.add(pobierzRowerZPliku);
         }
-        for (Rower r:rowers
-             ) {
-            System.out.println(r.getNazwa());
-            System.out.println(r.getMarka());
-            System.out.println(r.getKolor());
-            System.out.println(r.getOpony());
-            System.out.println(r.getPrzerzutki());
-            System.out.println(r.getRodzaj());
-        }
-
+        scanner.close();
+        return rowers;
     }
 }
